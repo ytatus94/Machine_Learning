39e8c84935358e4e08b369ff13f99b96bac50cf6
Yu-Ting <ytshen@iMac.local>
2022-03-06 15:49:49 -0800

    Remove BitTiger_DS501

--- BitTiger_DS501/DS501_PyAlgo_linkedlist_cycle.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def hasCycle(self, head):
-        fast, slow = head, head
-        while fast and fast.next:
-            fast, slow = fast.next.next, slow.next
-
-            if fast == slow
-                return True
-        return False
